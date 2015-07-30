b795f44eed07e288da09188ae3a300360fefc53a
Ingvar Stepanyan <me@rreverser.com>
2015-07-29 17:01:49 -0700

    Working version.

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import { AST_Node, Compressor, TreeWalker } from 'uglify-js';
+
+var compressor = Compressor();
+
+class LocationFixer extends TreeWalker {
+	constructor(path) {
+		super(node => {
+			node.start.file = node.end.file = path.hub.file.opts.filenameRelative;
+		});
+	}
+}
+
+export default ({ Plugin, types: t }) => new Plugin('uglify', {
+	visitor: {
+		Program(ast) {
+			// Convert to UglifyJS AST
+			var uAST = AST_Node.from_mozilla_ast(ast);
+
+			// Fix locations (Babel doesn't insert `loc.source` into nodes for some reason)
+			uAST.walk(new LocationFixer(this));
+
+			// Compress
+			uAST.figure_out_scope();
+			uAST = uAST.transform(compressor);
+
+			// Mangle names
+			uAST.figure_out_scope();
+			uAST.compute_char_frequency();
+			uAST.mangle_names();
+
+			// Convert back to ESTree AST
+			return uAST.to_mozilla_ast();
+		}
+	}
+});
